40be264e0af124dad36bbd0660c1f50e2b4ec75c
RinTrin <tomirin.rinta@gmail.com>
2021-11-14 00:29:19 +0900

    bin_search_and_union_find

---
+++ algorithm/binary_search.py
@@ -0,0 +1,18 @@
+
+
+from cv2 import MIXED_CLONE
+
+
+def binary_search(search_list, item):
+    low = 0
+    high = len(search_list)-1
+    while low <= high:
+        middle = (low + high)//2
+        if search_list[middle] == item:
+            return middle
+        elif search_list[middle] < item:
+            low = middle+1
+        else: #  search_list[middle] > item
+            high = middle-1
+    return None
+
